beb571c0cfd6a1307017359837250787594251a0
Tyler Collins <tjscollins@gmail.com>
2017-06-21 14:09:02 +1000

    Add and remove themes

--- wp-content/themes/edupress/functions/theme/config.php
+++
@@ -1,8 +0,0 @@
-<?php return array(
-/**
- * Theme configuration goes there
- */
-    'name'    => 'Edupress',    // theme name
-    'styled'  => false,                // true if current theme supports styling
-    'scripts' => array('tabber-minimized','simpletabs','dropdown','jcarousel')         // javascripts theme uses
-);
