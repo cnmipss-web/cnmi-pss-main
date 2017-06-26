0b29da849c966190012bd79fc9ac073b9aec4363
Tyler Collins <tjscollins@gmail.com>
2017-06-26 16:02:14 +1000

    Initial version of front-page.php and related styles

---
+++ wp-content/themes/cnmi-pss/front-page.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * This is the template that displays the front page by default.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package cnmi_scholars
+ */
+ get_header(); ?>
+
+ 	<div id="primary" class="content-area">
+ 		<main id="main" class="site-main" role="main">
+      <h1 class="sr-only">CNMI PSS Main Page</h1>
+      <div class="jumbotron container">
+        <div id="myCarousel" class="carousel slide">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div id="carousel-inner" class="carousel-inner">
+            <?php
+            // Loop goes here
+            $news = new WP_Query(array(
+              'posts_per_page' => 5,
+            ));
+            while($news->have_posts()):
+              $news->the_post();?>
+              <div class="item">
+                <h2 class="carousel-img-title"><?php the_title(); ?></h2>
+                <a title="Link to <?php the_title()?> article" href=<?php the_permalink() ?>>
+                  <?php the_post_thumbnail('full'); ?>
+                </a>
+              </div>
+            <?php endwhile;
+             ?>
+          </div>
+          <script>
+            document.getElementById("carousel-inner").children[0].className += ' active';
+          </script>
+
+          <!-- Left and right controls -->
+          <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row school-links">
+          <div class="col-xs-12">
+            <h2 class="section-title">Our Schools</h2>
+          </div>
+          <?php
+          cnmi_create_school_btns('Elementary School');
+          cnmi_create_school_btns('Middle School');
+          cnmi_create_school_btns('Jr Sr High School');
+          cnmi_create_school_btns('High School');
+           ?>
+        </div>
+      </div>
+      <br />
+      <div class="container">
+        <div class="row notice">
+          <div class="col-xs-12">
+            <h2 class="section-title">Notice to Persons with Disabilities</h2>
+            <p class="section-text">
+              THE CNMI PSS’ current website content and functionality is not accessible to people with disabilities including but not limited to those who are blind, have low-vision, those with disabilities affecting fine motor control, or by those who are deaf. As such, these barriers deny persons with disabilities access to programs, services, and activities, offered on the website and may impede CNMI PSS’ communications with persons with disabilities.
+            </p>
+            <p class="section-text">
+              Anyone facing a barrier to access to the CNMI PSS’ website content and functionality can contact Associate Commissioner Glenn Muna at (670) 783-8683 or glenn.muna@cnmipss.org or his office at (670) 664-3820 and the requested functionality and or content will be made accessible and or available to you within 24 hours of your request. If you would like to file a formal grievance against the CNMI PSS pursuant to the requirements of Section 504 and Title II, you may submit such grievance to the Legal Counsel Office Attorneys: Adam Hardwicke (670) 783-8966 adam.hardwicke@cnmipss.org or Tiberius Mocanu (670) 287-8427 tiberius.mocanu@cnmipss.org.
+            </p>
+          </div>
+        </div>
+      </div>
+
+ 		</main><!-- #main -->
+ 	</div><!-- #primary -->
+
+ <?php
+ get_footer();
