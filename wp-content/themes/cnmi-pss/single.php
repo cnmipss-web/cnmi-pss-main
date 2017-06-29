09f7d5a469b27471eedfb775592b8df4a5a41e7f
Tyler Collins <tjscollins@gmail.com>
2017-06-29 10:09:51 +1000

    Styles for singe news item

--- wp-content/themes/cnmi-pss/single.php
+++ wp-content/themes/cnmi-pss/single.php
@@ -9,26 +9,33 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		while ( have_posts() ) : the_post();
-
-			get_template_part( 'template-parts/content', get_post_format() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<br />
+		<div class="container">
+			<?php
+			if ( have_posts() ) :
+				/* Start the Loop */
+				while (have_posts()) : the_post();?>
+				 <div class="row">
+					 <?php
+						/*
+						* Include the Post-Format-specific template for the content.
+						* If you want to override this in a child theme, then include a file
+						* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						*/
+						get_template_part( 'template-parts/content', get_post_format() );
+					 ?>
+				 </div>
+			 <?php
+				endwhile;
+				cnmi_posts_navigation();
+			else :
+				get_template_part( 'template-parts/content', 'none' );
+			endif; ?>
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_sidebar();
