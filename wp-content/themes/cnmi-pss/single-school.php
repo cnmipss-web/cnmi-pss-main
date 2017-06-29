604a8e1dedc3885fbb14c0faeece107182448263
Tyler Collins <tjscollins@gmail.com>
2017-06-29 11:11:34 +1000

    single-school template

---
+++ wp-content/themes/cnmi-pss/single-school.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package CNMI_PSS
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<br />
+      <div class="container">
+			<?php
+			while ( have_posts() ) : the_post();?>
+				<div class="row">
+					<?php get_template_part( 'template-parts/info', 'school' ); ?>
+				</div>
+			<?php
+			endwhile; // End of the loop.
+			?>
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
