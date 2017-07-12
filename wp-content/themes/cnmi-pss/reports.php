fd79294e41b8388af25688b4ddd8d975bea91bfb
Tyler Collins <tjscollins@gmail.com>
2017-07-12 14:29:45 +1000

    Bugfix: Do not display empty tabs

--- wp-content/themes/cnmi-pss/reports.php
+++ wp-content/themes/cnmi-pss/reports.php
@@ -16,73 +16,73 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-			<br />
-      <div class="container">
-				<?php
-				if(have_posts()):
-					the_post();?>
-				<div class="row">
-					<div class="col-xs-12">
-						<header>
-							<h1 class="page-title">
-								<?php the_title(); ?>
-							</h1>
-						</header>
-						<?php the_content(); ?>
-				 </div>
-				</div>
-
-				<div class="row">
-					<div class="col-xs-12">
-						<ul class="nav nav-tabs" role="tablist" id="tablist">
-							<?php
-							$tabs = cnmi_get_report_tabs();
-							foreach ($tabs as $this_tab) {
-								?>
-								<li role="presentation">
-									<a
-										href="<?php echo '#' . $this_tab['id']; ?>"
-										role="tab"
-										aria-controls="<?php echo $this_tab['id']; ?>"
-										data-toggle="tab">
-										<?php echo $this_tab['title']; ?>
-									</a>
-								</li>
-							<?php
-							}
-							?>
-						</ul>
-						<div class="tab-content">
-							<?php
-							foreach ($tabs as $this_tab) {
-								?>
-								<div role="tabpanel" class="tab-pane fade" id="<?php echo $this_tab['id']; ?>">
-									<?php echo $this_tab['content']; ?>
-								</div>
-								<?php
-							}
-							?>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-xs-12 col-sm-10 col-sm-push-1">
-						<?php get_template_part( 'template-parts/info', 'contact' ); ?>
-					</div>
-				</div>
-				<script>
-					// Set active tab and tabpanel
-					let firstTab = jQuery('#tablist').children()[0];
-					jQuery(firstTab).addClass('active').addClass('in');
-					let firstPane = jQuery(".tab-pane")[0];
-					jQuery(firstPane).addClass('active').addClass('in');
-				</script>
-			</div>
-		<?php endif;?>
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_footer();
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <br />
+            <div class="container">
+                <?php
+		if(have_posts()):
+		the_post();?>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <header>
+                                <h1 class="page-title">
+                                    <?php the_title(); ?>
+                                </h1>
+                            </header>
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <ul class="nav nav-tabs" role="tablist" id="tablist">
+                                <?php
+			        $tabs = cnmi_get_report_tabs();
+			        foreach ($tabs as $this_tab) {
+                                    if(strlen($this_tab['title']) > 0) {
+			        ?>
+                                    <li role="presentation">
+                                        <a href="<?php echo '#' . $this_tab['id']; ?>" role="tab" aria-controls="<?php echo $this_tab['id']; ?>" data-toggle="tab">
+                                            <?php echo $this_tab['title']; ?>
+                                        </a>
+                                    </li>
+                                <?php
+                                }
+			        }
+			        ?>
+                            </ul>
+                            <div class="tab-content">
+                                <?php
+			        foreach ($tabs as $this_tab) {
+			        ?>
+                                    <div role="tabpanel" class="tab-pane fade" id="<?php echo $this_tab['id']; ?>">
+                                        <?php echo $this_tab['content']; ?>
+                                    </div>
+                                <?php
+			        }
+			        ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-10 col-sm-push-1">
+                            <?php get_template_part( 'template-parts/info', 'contact' ); ?>
+                        </div>
+                    </div>
+                    <script>
+                     // Set active tab and tabpanel
+                     let firstTab = jQuery('#tablist').children()[0];
+                     jQuery(firstTab).addClass('active').addClass('in');
+                     let firstPane = jQuery(".tab-pane")[0];
+                     jQuery(firstPane).addClass('active').addClass('in');
+                    </script>
+            </div>
+                <?php endif;?>
+        </main>
+        <!-- #main -->
+    </div>
+    <!-- #primary -->
+    
+    <?php
+    get_footer();
