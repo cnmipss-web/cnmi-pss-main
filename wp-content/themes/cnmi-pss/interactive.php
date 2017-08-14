e47bd12bf0fb4f55c0e0690730b845f54232498c
Tyler Collins <tjscollins@gmail.com>
2017-08-14 23:57:44 +0000

    Clean up interactive template to work better with CLJS

--- wp-content/themes/cnmi-pss/interactive.php
+++ wp-content/themes/cnmi-pss/interactive.php
@@ -27,7 +27,20 @@ get_header(); ?>
                         <?php the_content(); ?>
                     </div>
                 </div>
-                <?php react_app_loading_component(); ?>                
+                <div class="row">
+                    <div id="react-app" class="searchable-table col-xs-12">
+                        <div id="floatingCirclesG">
+                            <div class="f_circleG" id="frotateG_01"></div>
+                            <div class="f_circleG" id="frotateG_02"></div>
+                            <div class="f_circleG" id="frotateG_03"></div>
+                            <div class="f_circleG" id="frotateG_04"></div>
+                            <div class="f_circleG" id="frotateG_05"></div>
+                            <div class="f_circleG" id="frotateG_06"></div>
+                            <div class="f_circleG" id="frotateG_07"></div>
+                            <div class="f_circleG" id="frotateG_08"></div>
+                        </div>
+                    </div>
+                </div>
                 <div class="row">
                     <div class="col-xs-12 col-sm-10 col-sm-push-1">
                         <?php get_template_part( 'template-parts/info', 'contact' ); ?>
