c0d17bb1377c76f20bfa4660994db6721772b534
Tyler Collins <tjscollins@gmail.com>
2017-09-13 05:57:51 +0000

    Tweak to More news links, moved OCR notice to footer template

--- wp-content/themes/cnmi-pss/front-page.php
+++ wp-content/themes/cnmi-pss/front-page.php
@@ -57,20 +57,20 @@ get_header(); ?>
                     <div class="col-xs-2">
                         <div class="col-xs-12">More News:</p></div>
                     </div>
+                    <div class="col-xs-10">
+                        <?php
+                        while($news_links->have_posts()) {
+                            $news_links->the_post(); ?>
+                            <div class="col-xs-12 col-sm-4 text-center" >
+                                <a title="Link to article" href=<?php the_permalink() ?>>
+                                    <?php the_title() ?>
+                                </a>
+                            </div>
+                        <?php
+                        } ?>
+                    </div>
                 <?php
                 } ?>
-                <div class="col-xs-10">
-                    <?php
-                    while($news_links->have_posts()) {
-                        $news_links->the_post(); ?>
-                        <div class="col-xs-12 col-sm-4 text-center" >
-                            <a title="Link to article" href=<?php the_permalink() ?>>
-                                <?php the_title() ?>
-                            </a>
-                        </div>
-                    <?php
-                    } ?>
-                </div>
             </div>
             <!-- <div class="row">
                  <div class="col-xs-10">
@@ -169,20 +169,6 @@ get_header(); ?>
             </div>
         </div>
         <br />
-        <div class="container">
-            <div class="row notice">
-                <div class="col-xs-12">
-                    <h2 class="section-title">Notice to Persons with Disabilities</h2>
-                    <p class="section-text">
-                        THE CNMI PSS’ current website content and functionality is not accessible to people with disabilities including but not limited to those who are blind, have low-vision, those with disabilities affecting fine motor control, or by those who are deaf. As such, these barriers deny persons with disabilities access to programs, services, and activities, offered on the website and may impede CNMI PSS’ communications with persons with disabilities.
-                    </p>
-                    <p class="section-text">
-                        Anyone facing a barrier to access to the CNMI PSS’ website content and functionality can contact Associate Commissioner Glenn Muna at (670) 783-8683 or glenn.muna@cnmipss.org or his office at (670) 664-3820 and the requested functionality and or content will be made accessible and or available to you within 24 hours of your request. If you would like to file a formal grievance against the CNMI PSS pursuant to the requirements of Section 504 and Title II, you may submit such grievance to the Legal Counsel Office Attorneys: Adam Hardwicke (670) 783-8966 adam.hardwicke@cnmipss.org or Tiberius Mocanu (670) 287-8427 tiberius.mocanu@cnmipss.org.
-            </p>
-          </div>
-        </div>
-      </div>
-
     </main><!-- #main -->
   </div><!-- #primary -->
 
