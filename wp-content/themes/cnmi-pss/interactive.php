3a1eaac137291a4e122f7f8697b4f6ad65d2c5d0
Tyler Collins <tjscollins@gmail.com>
2017-09-27 00:29:58 +0000

    Simple fallback message for users without JS

--- wp-content/themes/cnmi-pss/interactive.php
+++ wp-content/themes/cnmi-pss/interactive.php
@@ -11,7 +11,7 @@
 
 get_header(); ?>
 <div id="primary" class="content-area">
-    <main id="main" class="site-main">
+    <main id="main" class="site-main" role="main">
         <br />
         <div class="container">
             <?php
@@ -29,16 +29,14 @@ get_header(); ?>
                 </header>
                 <div class="row">
                     <div id="react-app" class="searchable-table col-xs-12">
-                        <div id="floatingCirclesG">
-                            <div class="f_circleG" id="frotateG_01"></div>
-                            <div class="f_circleG" id="frotateG_02"></div>
-                            <div class="f_circleG" id="frotateG_03"></div>
-                            <div class="f_circleG" id="frotateG_04"></div>
-                            <div class="f_circleG" id="frotateG_05"></div>
-                            <div class="f_circleG" id="frotateG_06"></div>
-                            <div class="f_circleG" id="frotateG_07"></div>
-                            <div class="f_circleG" id="frotateG_08"></div>
-                        </div>
+                        <noscript>
+                            <div class="row" ><div class="col-xs-12 col-sm-10 col-sm-push-1" >
+                                <p>This page contains interactive content which requires Javascript to function properly.  Please enable Javascript or switch to using a user agent which is capable of rendering Javascript content.  If you do not have or are unable to use a user agent with Javascript functionality, please contact us using one of the methods listed below to request the information you were seeking.</p>
+                            </div></div>
+                        </noscript>
+                        <script>
+                         document.getElementById('react-app').innerHTML = '<div id="floatingCirclesG"><div class="f_circleG" id="frotateG_01"></div><div class="f_circleG" id="frotateG_02"></div><div class="f_circleG" id="frotateG_03"></div><div class="f_circleG" id="frotateG_04"></div><div class="f_circleG" id="frotateG_05"></div><div class="f_circleG" id="frotateG_06"></div><div class="f_circleG" id="frotateG_07"></div><div class="f_circleG" id="frotateG_08"></div></div>';
+                        </script>
                     </div>
                 </div>
                 <div class="row">
