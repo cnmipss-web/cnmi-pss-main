c405f6592929aea307fa7b6976086f662339568c
Tyler Collins <tjscollins@gmail.com>
2017-10-02 00:17:45 +0000

    Password login for test server

--- wp-login.php
+++ wp-login.php
@@ -899,6 +899,19 @@ default:
 ?>
 
 <form name="loginform" id="loginform" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>" method="post">
+	<p>
+        <label for="user_login">
+			<?php _e( 'Username or Email Address' ); ?>
+			<br />
+        	<input type="text" name="log" id="user_login"<?php echo $aria_describedby_error; ?> class="input" value="<?php echo esc_attr($user_login ); ?>" size="20" />
+ 		</label>
+    </p>
+    <p>
+        <label for="user_pass"><?php _e( 'Password' ); ?><br />
+        	<input type="password" name="pwd" id="user_pass"<?php echo $aria_describedby_error; ?> class="input" value="" size="20" />
+		</label>
+    </p>
+
 	<?php
 	/**
 	 * Fires following the 'Password' field in the login form.
@@ -907,6 +920,23 @@ default:
 	 */
 	do_action( 'login_form' );
 	?>
+
+       <p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php checked
+( $rememberme ); ?> /> <?php esc_html_e( 'Remember Me' ); ?></label></p>
+       <p class="submit">
+               <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Log 
+In'); ?>" />
+<?php  if ( $interim_login ) { ?>
+               <input type="hidden" name="interim-login" value="1" />
+<?php  } else { ?>
+               <input type="hidden" name="redirect_to" value="<?php echo esc_attr($redirect_to); ?>" />
+<?php  } ?>
+<?php   if ( $customize_login ) : ?>
+               <input type="hidden" name="customize-login" value="1" />
+<?php   endif; ?>
+               <input type="hidden" name="testcookie" value="1" />
+       </p>
+
 </form>
 
 <script type="text/javascript">
