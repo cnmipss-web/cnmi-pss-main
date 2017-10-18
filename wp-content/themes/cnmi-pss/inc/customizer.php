f27d79c0f040a786861f848c47c3c1bf21946c83
Tyler Collins <tjscollins@gmail.com>
2017-10-18 22:09:22 +0000

    Remove customization functionality from cnmi-pss theme

--- wp-content/themes/cnmi-pss/inc/customizer.php
+++ wp-content/themes/cnmi-pss/inc/customizer.php
@@ -6,21 +6,7 @@
  */
 
 /**
- * Add postMessage support for site title and description for the Theme Customizer.
- *
- * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ * Remove all customization functionality from this theme
  */
-function cnmi_pss_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-}
-add_action( 'customize_register', 'cnmi_pss_customize_register' );
-
-/**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
- */
-function cnmi_pss_customize_preview_js() {
-	wp_enqueue_script( 'cnmi_pss_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
-}
-add_action( 'customize_preview_init', 'cnmi_pss_customize_preview_js' );
+remove_all_actions('customize_register');
+remove_all_actions('customize_preview_init');
