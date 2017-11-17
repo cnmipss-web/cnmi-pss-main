28d15f9c4fae4c5817b9af2a0b7a5d2657cc8f63
Tyler Collins <tjscollins@gmail.com>
2017-11-17 05:24:57 +0000

    Improve BrokenLinks and ImageSource to use cached data from scrapy and return the results of testing all entries in that data

--- test/cnmipss_tests/test_valid_urls.py
+++ test/cnmipss_tests/test_valid_urls.py
@@ -11,19 +11,43 @@ SETTINGS = get_project_settings()
 
 class BrokenLinks(unittest.TestCase):
 
-    def test_links(self):
+    def setUp(self):
+        self.broken_links = []
+
+    def tearDown(self):
+        self.assertEqual([], self.broken_links,
+                         msg="Broken links found")
+
+    def test_no_broken_links(self):
         link_file = open(SETTINGS['LINK_DATA'], 'r')
         for link in link_file.readlines():
             link_data = json.loads(link)
-            self.assertLess(link_data['status'], 400)
+            err_msg = 'Broken link: ' + str(link_data)
+            try:
+                self.assertLess(link_data['status'], 400,
+                                msg=err_msg)
+            except AssertionError as err:
+                self.broken_links.append(err)
 
 
 class ImageSources(unittest.TestCase):
 
-    def test_img_sources(self):
+    def setUp(self):
+        self.bad_image_sources = []
+
+    def tearDown(self):
+        self.assertEqual([], self.bad_image_sources,
+                         msg="Broken image sources found")
+
+    def test_no_broken_img_sources(self):
         link_file = open(SETTINGS['IMG_DATA'], 'r')
         link_set = set()
         for data in link_file.readlines():
             json_data = json.loads(data)
             for img in json_data['sources']:
-                self.assertLess(img['status'], 400)
+                err_msg = 'Broken image source ' + str(img)
+                try:
+                    self.assertLess(img['status'], 400,
+                                    msg=err_msg)
+                except AssertionError as err:
+                    self.bad_image_sources.append(err)
