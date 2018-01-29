39507196481b9f273720cde3b08d839a5f357843
Tyler Collins <tjscollins@gmail.com>
2018-01-28 16:03:57 -1000

    WordPress update to version 4.9.2

--- wp-login.php
+++ wp-login.php
@@ -984,7 +984,6 @@ default:
 ?>
 
 <form name="loginform" id="loginform" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>" method="post">
-<!--
 	<p>
 		<label for="user_login"><?php _e( 'Username or Email Address' ); ?><br />
 		<input type="text" name="log" id="user_login"<?php echo $aria_describedby_error; ?> class="input" value="<?php echo esc_attr( $user_login ); ?>" size="20" /></label>
@@ -993,7 +992,6 @@ default:
 		<label for="user_pass"><?php _e( 'Password' ); ?><br />
 		<input type="password" name="pwd" id="user_pass"<?php echo $aria_describedby_error; ?> class="input" value="" size="20" /></label>
 	</p>
--->
 	<?php
 	/**
 	 * Fires following the 'Password' field in the login form.
@@ -1002,7 +1000,6 @@ default:
 	 */
 	do_action( 'login_form' );
 	?>
-<!--
 	<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php checked( $rememberme ); ?> /> <?php esc_html_e( 'Remember Me' ); ?></label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Log In'); ?>" />
@@ -1016,7 +1013,6 @@ default:
 <?php   endif; ?>
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
--->
 </form>
 
 <?php if ( ! $interim_login ) { ?>
