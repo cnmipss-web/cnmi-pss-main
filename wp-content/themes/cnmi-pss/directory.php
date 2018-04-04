a77d137bff1962fdf090c38276eca8167a5828b8
Tyler Collins <tjscollins@gmail.com>
2018-04-04 13:58:30 -1000

    Central Office Directory template and associated SCSS styles

--- wp-content/themes/cnmi-pss/directory.php
+++ wp-content/themes/cnmi-pss/directory.php
@@ -33,82 +33,122 @@ $current_office = "";
         <br />
         <div class="container">
             <div class="row" style="">
-                <div class="col-xs-12 col-sm-10 sm-push-1">
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="page-entry-header">
-                    <?php the_title( '<h1 class="page-entry-title">', '</h1>' ); ?>
-                    </header><!-- .entry-header -->
-                    
-                    <div class="page-entry-content col-xs-12 col-sm-10 col-sm-push-1">
-                    <?php
-                    the_content();
-                    wp_link_pages( array(
-                        'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'cnmi-pss' ),
-                        'after'  => '</div>',
-                    ) );
-                    ?>
-                    </div><!-- .entry-content -->
-                    
-                    <?php if ( get_edit_post_link() ) : ?>
-                    <footer class="page-entry-footer col-xs-12">
+                <div class="col-xs-12">
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <header class="page-entry-header">
+                        <?php the_title( '<h1 class="page-entry-title">', '</h1>' ); ?>
+                        </header><!-- .entry-header -->
+                        
+                        <div class="page-entry-content col-xs-12 col-sm-10 col-sm-push-1">
                         <?php
-                        edit_post_link(
-                        sprintf(
-                            wp_kses(
-                            /* translators: %s: Name of current post. Only visible to screen readers */
-                            __( 'Edit <span class="screen-reader-text">%s</span>', 'cnmi-pss' ),
-                            array(
-                                'span' => array(
-                                'class' => array(),
-                                ),
-                            )
-                            ),
-                            get_the_title()
-                        ),
-                        '<span class="edit-link">',
-                        '</span>'
-                        );
+                        the_content();
+                        wp_link_pages( array(
+                            'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'cnmi-pss' ),
+                            'after'  => '</div>',
+                        ) );
                         ?>
-                    </footer><!-- .entry-footer -->
-                    <?php endif; ?>   
-                </article>
+                        </div><!-- .entry-content -->
+                        <footer class="page-entry-content col-xs-12 col-sm-10 col-sm-push-1" >
+                            <!-- <i id="all-caret"
+                                    class="fa fa-caret-down"></i> -->
+                            <button id="all-caret-button"
+                                class="btn btn-default"
+                                onclick="flipAll()"
+                                data-expanded="true">
+                                Collapse All
+                            </button>    
+                        </footer>   
+                    </article>
+                    <div style="display: none;">
+                        <table>
             <?php
             while($directory_query->have_posts()):
                 $directory_query->the_post();?>
                   
                 <?php 
-                if ($current_office != get_field('office')) {?>
-                    <?php 
-                    $current_office = get_field('office');?>
-                    </table>
+                if ($current_office != sanitize_title(get_field('office'))) {
+                    $current_office = sanitize_title(get_field('office'));
+                    ?>
+                        </table>
+                    </div>
                 </div>
             </div>
-            <div class="row">
+            <div class="row directory-entry">
                 <div class="col-xs-12 col-sm-10 col-sm-push-1">
-                    <header class="page-entry-header" >
-                        <h2><?php the_field('office') ?></h2>
+                    <header class="page-entry-header directory-entry__header" >
+                        <i id="<?php echo $current_office?>-caret"
+                                class="fa fa-caret-down"></i>
+                        <a
+                        role="button"
+                        href="#<?php echo $current_office ?>-table"
+                        data-toggle="collapse"
+                        aria-controls="<?php echo $current_office ?>-table"
+                        aria-expanded="true"
+                        onclick="flipCaret('#<?php echo $current_office?>-caret')">
+                            <h2><?php the_field('office') ?></h2>
+                        </a>    
                     </header>
-                    <table class="directory-table">
-                        <tr class="directory-table__header-row">
-                            <th scope="col">Name</th>
-                            <th scope="col">Position</th>
-                            <th scope="col">Telephone</th>
-                            <th scope="col">Email</th>
-                        </tr>
+                    <div class=" collapse in"
+                        id="<?php echo $current_office ?>-table">
+                        <table class="directory-table">
+                            <thead>
+                                <tr class="directory-table__header-row">
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Position</th>
+                                    <th scope="col">Telephone</th>
+                                    <th scope="col">Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
                 <?php } ?>
-                        <tr class="directory-table__entry-row">
-                            <td> <?php the_title(); ?> </td>
-                            <td> <?php the_field('job_title'); ?> </td>
-                            <td> <?php the_field('telephone'); ?> </td>
-                            <td> <?php the_field('email'); ?> </td>
-                        </tr>
+                                <tr class="directory-table__entry-row">
+                                    <td> <?php the_title(); ?> </td>
+                                    <td> <?php the_field('job_title'); ?> </td>
+                                    <td> <?php the_field('telephone'); ?> </td>
+                                    <td> <?php the_field('email'); ?> </td>
+                                </tr>
             <?php
              endwhile;?>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </main>
+    <script> 
+        function flipAll() {
+            var $ = jQuery;
+            var button = $("#all-caret-button");
+            var offices = $('header.directory-entry__header > a[role="button"][data-toggle="collapse"]');
+
+            if (button.attr('data-expanded') == 'true') {
+                offices.filter(function() {
+                    return ($(this).attr('aria-expanded') == 'true')
+                }).click();
+                button.html("Expand All")
+                button.attr('data-expanded', 'false');
+            } else {
+                offices.filter(function() {
+                    return ($(this).attr('aria-expanded') == 'false')
+                }).click();
+                button.html("Collapse All")
+                button.attr('data-expanded', 'true');
+            }
+
+        }
+
+        function flipCaret(id) {
+            var el = jQuery(id);
+            if (el.hasClass("fa-caret-down")) {
+                jQuery(id).removeClass("fa-caret-down");
+                jQuery(id).addClass("fa-caret-right");
+            } else {
+                jQuery(id).removeClass("fa-caret-right");
+                jQuery(id).addClass("fa-caret-down");                
+            }
+        }
+    </script>
     <!-- #main -->
 </div>
 <!-- #primary -->
