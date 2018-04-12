80a4bca7db3984d58ad0bb5920c1d5546e981252
Tyler Collins <tjscollins@gmail.com>
2018-04-12 12:42:53 -1000

    Bugfix: Add job title to info-contact.php template

--- wp-content/themes/cnmi-pss/template-parts/info-contact.php
+++ wp-content/themes/cnmi-pss/template-parts/info-contact.php
@@ -16,8 +16,9 @@ $include = get_field('include_info');
 
 foreach ($contacts as $entry) {
     $name = $entry->post_title;
+    $job_title = get_field('job_title', $entry->ID);
     if(strlen($name) > 0) { ?>
-        <p><?php echo $name; ?></p>
+        <p><?php echo($name . ", " . $job_title); ?></p>
         <?php 
         cnmi_contact_info($entry, $include);
     }
